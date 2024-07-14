83790b37504bd0b5ba255094117207bccb4486d4
ewnike <wpltd5@yahoo.com>
2024-07-13 23:04:52 -0500

    formatted py code with RUFF.

--- commodity_analysis.py
+++ commodity_analysis.py
@@ -1,10 +1,18 @@
+"""
+Eric Winiecke April, 2017
+Python code for commodity analysis
+using tickwrite_process_ticks and
+make_commodity_bars.
+"""
+
 import tables
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
-matplotlib.style.use('ggplot')
+matplotlib.style.use("ggplot")
+
 
 class com_Regression:
     """
@@ -18,85 +26,88 @@ class com_Regression:
             self.comm1 = comm1
         else:
             self.comm1 = input("Enter the first commodity you want to analyze: ")
-            
+
         if comm2 is not None:
             self.comm2 = comm2
         else:
             self.comm2 = input("Enter the second commodity you want to analyze: ")
-        
-        self.data = tables.open_file("TD_HistoricalBars.h5", mode='r')
+
+        self.data = tables.open_file("TD_HistoricalBars.h5", mode="r")
         if time_in_seconds is not None:
             self.time_in_seconds = time_in_seconds
         else:
-            self.time_in_seconds = int(input("Enter the length of time, in seconds, to include in each bar: "))
-            
+            self.time_in_seconds = int(
+                input("Enter the length of time, in seconds, to include in each bar: ")
+            )
+
         self.bars_comm1, self.bars_comm2 = self.read_bars()
-        
+
     def read_bars(self):
         arr_comm1 = self.data.get_node(("/TD_HistBars/{}").format(self.comm1)).read()
         arr_comm2 = self.data.get_node(("/TD_HistBars/{}").format(self.comm2)).read()
         return arr_comm1, arr_comm2
 
     def scatter(self):
-        comm1_open = self.bars_comm1['open_p']
-        comm2_open = self.bars_comm2['open_p']
+        comm1_open = self.bars_comm1["open_p"]
+        comm2_open = self.bars_comm2["open_p"]
         plt.scatter(comm1_open, comm2_open)
         plt.show()
-        
+
     def regression(self):
-        comm1_open = self.bars_comm1['open_p']
-        comm2_open = self.bars_comm2['open_p']
-        
-        comm1_time = self.bars_comm1.astype(h5_bar_type)['time']
-        
+        comm1_open = self.bars_comm1["open_p"]
+        comm2_open = self.bars_comm2["open_p"]
+
+        comm1_time = self.bars_comm1.astype(h5_bar_type)["time"]
+
         regr = linear_model.LinearRegression()
-        regr.fit(comm1_time.reshape((-1,1)), comm1_open.reshape((-1,1)))
-        
-        line_y = regr.predict(comm1_time.reshape((-1,1)))
-        
+        regr.fit(comm1_time.reshape((-1, 1)), comm1_open.reshape((-1, 1)))
+
+        line_y = regr.predict(comm1_time.reshape((-1, 1)))
+
         fig, ax1 = plt.subplots()
-        ax1.scatter(comm1_time, comm1_open, color='black')
-        ax1.plot(comm1_time, line_y, color='blue', linewidth=3)
-       
-        comm2_time = self.bars_comm2.astype(h5_bar_type)['time']
+        ax1.scatter(comm1_time, comm1_open, color="black")
+        ax1.plot(comm1_time, line_y, color="blue", linewidth=3)
+
+        comm2_time = self.bars_comm2.astype(h5_bar_type)["time"]
         regr = linear_model.LinearRegression()
-        regr.fit(comm2_time.reshape((-1,1)), comm2_open.reshape((-1,1)))
-        
-        line_y = regr.predict(comm2_time.reshape((-1,1)))
-        print('Coefficients: \n', regr.coef_)
-        
+        regr.fit(comm2_time.reshape((-1, 1)), comm2_open.reshape((-1, 1)))
+
+        line_y = regr.predict(comm2_time.reshape((-1, 1)))
+        print("Coefficients: \n", regr.coef_)
+
         ax2 = ax1.twinx()
-        ax2.scatter(comm2_time, comm2_open, color='green')
-        ax2.plot(comm2_time, line_y, color='red', linewidth=3)
-        
+        ax2.scatter(comm2_time, comm2_open, color="green")
+        ax2.plot(comm2_time, line_y, color="red", linewidth=3)
+
         fig.tight_layout()
         plt.show()
-        
+
     def get_stats(self):
         comm1_sum = 0
         comm2_sum = 0
         cov_sum = 0
-        
-        comm1_open = self.bars_comm1['open_p']
-        comm2_open = self.bars_comm2['open_p']
+
+        comm1_open = self.bars_comm1["open_p"]
+        comm2_open = self.bars_comm2["open_p"]
         comm1_mean = np.mean(comm1_open)
         comm2_mean = np.mean(comm2_open)
-        
+
         for i in range(len(comm1_open)):
             comm1_diff = comm1_open[i] - comm1_mean
             comm2_diff = comm2_open[i] - comm2_mean
             cov_sum += comm1_diff * comm2_diff
-            comm1_sum += comm1_diff ** 2
-            comm2_sum += comm2_diff ** 2
-   
+            comm1_sum += comm1_diff**2
+            comm2_sum += comm2_diff**2
+
         covariance = cov_sum / (len(comm1_open) - 1)
         sigma_comm1 = np.sqrt(comm1_sum / (len(comm1_open) - 1))
         sigma_comm2 = np.sqrt(comm2_sum / (len(comm1_open) - 1))
         correlation = covariance / (sigma_comm1 * sigma_comm2)
         r_coef = np.corrcoef(comm1_open, comm2_open)
-        
+
         return covariance, sigma_comm1, sigma_comm2, correlation, r_coef
-    
+
+
 if __name__ == "__main__":
     a = com_Regression()
     print(a.get_stats())
